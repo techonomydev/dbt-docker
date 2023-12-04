8318180e0edcffd811cdd3067ea29c2f21fa807e
Nils Golembiewski <nilsgolembiewski@techonomy.nl>
2023-12-04 13:41:55 +0100

    cd after generating report

--- scripts/elementary-serve.bash
+++ scripts/elementary-serve.bash
@@ -9,7 +9,6 @@ port="${ELEMENTARY_SERVE_PORT:-8080}"
 
 # Create working directory
 workdir="$(mktemp -d)"
-cd $workdir
 
 # Make sure temporary directory is cleaned up
 rm_workdir() {
@@ -18,8 +17,9 @@ rm_workdir() {
 trap rm_workdir EXIT
 
 # Generate edr report, passing all cli arguments to edr
-edr report --file-path index.html $@
+edr report --file-path "$workdir/index.html" $@
 
 # Serve the report on chosen port
+cd $workdir
 python -m http.server $port
 
